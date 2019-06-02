9ac7985a5f7af060a7ad4590b18dc2363e264180
Zia Malik <ziamalik@gmail.com>
2019-06-02 19:49:34 -0400

    Corrected typos

--- BlockchainController.js
+++ BlockchainController.js
@@ -54,7 +54,7 @@ class BlockchainController {
         });
     }
 
-    // Endpoint that allow Submit a Star, yu need first to `requestOwnership` to have the message (POST endpoint)
+    // Endpoint that allow Submit a Star, you need first to `requestOwnership` to have the message (POST endpoint)
     submitStar() {
         this.app.post("/submitstar", async (req, res) => {
             if(req.body.address && req.body.message && req.body.signature && req.body.star) {
